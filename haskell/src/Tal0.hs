49837051048309ece5d33bc8e045b2b3b41fab62
elpinal <elpinal@users.noreply.github.com>
2018-02-21 22:32:11 +0900

    Add a type synonym, Label, to Int

--- haskell/src/Tal0.hs
+++ haskell/src/Tal0.hs
@@ -4,9 +4,11 @@ import qualified Data.Map.Lazy as Map
 
 newtype Register = Register Int
 
+type Label = Int
+
 data Operand
   = Int Int
-  | Label Int
+  | Label Label
   | Reg Register
 
 data Inst
@@ -18,4 +20,4 @@ data Seq = Seq [Inst] Operand
 
 type File = Map.Map Register Operand
 
-type Heap = Map.Map Int Seq
+type Heap = Map.Map Label Seq
