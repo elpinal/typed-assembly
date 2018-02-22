f1ddfba38876164b76d90696e129d1e1671c212d
elpinal <elpinal@users.noreply.github.com>
2018-02-22 16:32:47 +0900

    Branch on a minimal number of arguments

--- haskell/src/Tal0.hs
+++ haskell/src/Tal0.hs
@@ -49,17 +49,18 @@ eval1 m @ Machine
   , file = f
   , current = s
   } = case s of
-  Seq [] o                  -> fmap update $ fetch o f >>= fromLabel >>= flip Map.lookup h
-  Seq (Mov r o : is) o0     -> (updateReg r $ Seq is o0) <$> fetch o f
-  Seq (Add rd rs o : is) o0 -> do
-    n1 <- Map.lookup rs f >>= fromInt
-    n2 <- fetch o f >>= fromInt
-    return . (updateReg rd $ Seq is o0) . Int $ n1 + n2
-  Seq (IfJump r o : is) o0  -> do
-    n <- Map.lookup r f >>= fromInt
-    if n == 0
-      then fmap update $ fetch o f >>= fromLabel >>= flip Map.lookup h
-      else return . update $ Seq is o0
+  Seq [] o        -> fmap update $ fetch o f >>= fromLabel >>= flip Map.lookup h
+  Seq (i : is) o0 -> let rest = Seq is o0 in case i of
+    Mov r o     -> updateReg r rest <$> fetch o f
+    Add rd rs o -> do
+      n1 <- Map.lookup rs f >>= fromInt
+      n2 <- fetch o f >>= fromInt
+      return . updateReg rd rest . Int $ n1 + n2
+    IfJump r o  -> do
+      n <- Map.lookup r f >>= fromInt
+      if n == 0
+        then fmap update $ fetch o f >>= fromLabel >>= flip Map.lookup h
+        else return $ update rest
   where
     update s = m { current = s }
     updateReg r s o = m { file = Map.insert r o f, current = s }
