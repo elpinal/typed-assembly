38b306453344213ef6bd575e2769d45c8cf28e2a
elpinal <elpinal@users.noreply.github.com>
2018-02-22 22:52:37 +0900

    Define a lexer

--- haskell/src/Tal0/Parser.hs
+++ haskell/src/Tal0/Parser.hs
@@ -1,6 +1,18 @@
 module Tal0.Parser
   () where
 
+import Text.Parsec
+import Text.Parsec.Language
+import Text.Parsec.Token
+
 data Token
   = Ident
   | Jump
+
+def :: LanguageDef st
+def = emptyDef
+  { reservedNames = ["jump"]
+  }
+
+lexer :: TokenParser st
+lexer = makeTokenParser def
